2a2f7c35236fce113651b7f75dc82edf9d4eac07
alisonmundfrom <alison.mundfrom@outlook.com>
2022-09-01 18:36:19 -0500

    Height Averages
    This program calculates the average height of 5 students

---
+++ Averages.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+public class Averages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int heightOne = 60;
+		int heightTwo = 61;
+		int heightThree = 62;
+		int heightFour = 63;
+		int heightFive = 64;
+		
+		int sum = heightOne + heightTwo + heightThree + heightFour + heightFive;
+		
+		int avg = sum/5;
+		
+		System.out.println ("The average height is: " + avg +" inches");	
+				
+	}
+
+}
